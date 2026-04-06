de597f417deb9903d3cb3cdcde51a003b711933a
Somad Yessoufou <yessouf2009@yahoo.fr>
2026-04-06 16:42:01 +0000

    Docker configurations

---
+++ Services/Identity/Surveillance.Identity.Infrastructure/Data/UserDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Surveillance.Identity.Infrastructure.Data
+{
+    internal class UserDbContextFactory : IDesignTimeDbContextFactory<UserDbContext>
+    {
+        public UserDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UserDbContext>();
+
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Surveillance_DB;User Id=sa;Password=Admin@2025!;TrustServerCertificate=True;");
+
+            return new UserDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Services/Notification/Surveillance.Notification.Infrastructure/Data/NotificationDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Surveillance.Notification.Infrastructure.Data
+{
+    internal class NotificationDbContextFactory : IDesignTimeDbContextFactory<NotificationDbContext>
+    {
+        public NotificationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<NotificationDbContext>();
+
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Surveillance_DB;User Id=sa;Password=Admin@2025!;TrustServerCertificate=True;");
+
+            return new NotificationDbContext(optionsBuilder.Options);
+        }
+    }
+}
